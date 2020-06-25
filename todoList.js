c754f5e632377b818780dc2d9e62b90f68e4497f
Peter D'Elia <petertdelia@gmail.com>
2020-06-25 10:46:41 -0400

    first working copy

---
+++ todoList.js
@@ -0,0 +1,102 @@
+"use strict";
+
+class Todo {
+  static DONE_MARKER = "X";
+  static UNDONE_MARKER = " ";
+
+  constructor(title) {
+    this.title = title;
+    this.done = false;
+  }
+
+  toString() {
+    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
+    return `[${marker}] ${this.title}`;
+  }
+
+  markDone() {
+    this.done = true;
+  }
+
+  markUndone() {
+    this.done = false;
+  }
+
+  isDone() {
+    return this.done;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(todo) {
+    if (!(todo instanceof Todo)) {
+      throw new TypeError("can only add Todo objects");
+    }
+    
+    this.todos.push(todo);
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  markDoneAt(index) {
+    this.todos[index].markDone();
+  }
+
+  markUndoneAt(index) {
+    this.todos[index].markUndone();
+  }
+
+  isDone() {
+    return this.todos.every(todo => todo.isDone());
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop();
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1)[0];
+  }
+
+  toString() {
+    let returnStr = `---- Today's Todos ----`;
+    this.todos.forEach(todo => returnStr += `\n${todo}`);
+    return returnStr;
+  }
+
+  _validateIndex(index) {
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+}
+
